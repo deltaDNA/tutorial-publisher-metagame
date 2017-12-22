64d55acb3678159317be69c6196f7ccdbf0c41cd
LaurieMcCulloch <laurie.mcculloch@ntlworld.com>
2017-12-22 22:19:15 +0000

    Engage Campaigns added on Publisher MetaGame
    Engage Campaigns plugged in for publisher MetaGame, and a variety of signposting, rewarding and deeplinkg camapigns setup with targeted selection and cross promotions.

--- Assets/Scripts/metagame_sender.cs
+++ Assets/Scripts/metagame_sender.cs
@@ -12,6 +12,7 @@ namespace metagameTutorial
         // This is the EnvironmentKey and REST API URL for the "Publisher MetaGame" created on the deltaDNA platform
         private const string ENVIRONMENT_KEY                = "49994906769133941928339970015138";
         private const string COLLECT_REST_API_URL           = "https://collect12660pblsh.deltadna.net/collect/api";
+        private const string ENGAGE_REST_API_URL            = "https://engage12660pblsh.deltadna.net";
         private const string GAME_ID                        = "1";
         private const string GAME_NAME                      = "Simple Game";
         private const string GAME_GENRE                     = "Shootem Up";
@@ -56,17 +57,7 @@ namespace metagameTutorial
             ////////////////////////////////////////////////////////////////
             // Upload gameStarted and clientDevice Publisher MetaGame events
             StartCoroutine(UploadMetaGameEvents());
-        }
-
-
-        ////////////////////////////////////////////////////////////////////
-        // Publisher MetaGame Campaign Check 
-        public void MetaGameCampaignCheck()
-        {
-            DeltaDNA.Logger.LogDebug("MetaGame Campaign Check Click");
-        }
-
-
+        }        
 
 
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -216,5 +207,88 @@ namespace metagameTutorial
             }
             return null;    // Collect will insert a timestamp for us.
         }
+
+
+        ////////////////////////////////////////////////////////////////////
+        // Publisher MetaGame Engage Campaign Check  
+        ////////////////////////////////////////////////////////////////////
+        public void MetaGameRequestEngagement(Engagement engagement, Action<Engagement> onCompleted, Action<Exception> onError)
+        {
+            DeltaDNA.Logger.LogDebug("MetaGame Campaign Check Click");
+            if (String.IsNullOrEmpty(ENGAGE_REST_API_URL))
+            {
+                throw new Exception("Engage URL not configured.");
+            }
+
+            try
+            {
+                var dict = engagement.AsDictionary();
+
+                var request = new EngageRequest(dict["decisionPoint"] as string);
+                request.Flavour = dict["flavour"] as string;
+                request.Parameters = dict["parameters"] as Dictionary<string, object>;
+
+                EngageResponse handler = (string response, int statusCode, string error) => {
+                    engagement.Raw = response;
+                    engagement.StatusCode = statusCode;
+                    engagement.Error = error;
+
+                    onCompleted(engagement);
+                };
+
+                StartCoroutine(MetaGameEngageRequest(this, request, handler));
+
+            }
+            catch (Exception ex)
+            {
+                DeltaDNA.Logger.LogWarning("Engagement request failed: " + ex.Message);
+            }
+        }
+
+        internal static IEnumerator MetaGameEngageRequest(MonoBehaviour caller, EngageRequest request, EngageResponse response)
+        {
+            string requestJSON = request.ToJSON();
+            string url = String.Format("{0}/{1}", ENGAGE_REST_API_URL, ENVIRONMENT_KEY);
+
+            HttpRequest httpRequest = new HttpRequest(url);
+            httpRequest.HTTPMethod = HttpRequest.HTTPMethodType.POST;
+            httpRequest.HTTPBody = requestJSON;
+            httpRequest.TimeoutSeconds = DDNA.Instance.Settings.HttpRequestEngageTimeoutSeconds;
+            httpRequest.setHeader("Content-Type", "application/json");
+
+            System.Action<int, string, string> httpHandler = (statusCode, data, error) => {
+
+                string engagementKey = "DDSDK_ENGAGEMENT_" + request.DecisionPoint + "_" + request.Flavour;
+                if (error == null && statusCode >= 200 && statusCode < 300)
+                {
+                    try
+                    {
+                        PlayerPrefs.SetString(engagementKey, data);
+                    }
+                    catch (Exception exception)
+                    {
+                        DeltaDNA.Logger.LogWarning("Unable to cache engagement: " + exception.Message);
+                    }
+                }
+                else
+                {
+                    DeltaDNA.Logger.LogDebug("Engagement failed with " + statusCode + " " + error);
+                    if (PlayerPrefs.HasKey(engagementKey))
+                    {
+                        DeltaDNA.Logger.LogDebug("Using cached response");
+                        data = "{\"isCachedResponse\":true," + PlayerPrefs.GetString(engagementKey).Substring(1);
+                    }
+                    else
+                    {
+                        data = "{}";
+                    }
+                }
+
+                response(data, statusCode, error);
+            };
+
+            yield return caller.StartCoroutine(DeltaDNA.Network.SendRequest(httpRequest, httpHandler));
+        }
+
     }
 }
--- Assets/Scripts/metagame_tutorial.cs
+++ Assets/Scripts/metagame_tutorial.cs
@@ -13,10 +13,7 @@ namespace metagameTutorial
         public UnityEngine.UI.Text txtCoins;
 
         private int userLevel =  1;
-        private int userCoins = 100; 
-
-        metagame_sender MetaGameSender; 
-
+        private int userCoins =  0;       
 
         // Use this for initialization
         void Start()
@@ -28,7 +25,7 @@ namespace metagameTutorial
             // events to a deltaDNA game instance specifically for this game
 
             // For demo purposes... clear all data and treat each run as if it were a fresh new install.
-            DDNA.Instance.ClearPersistentData(); 
+            //DDNA.Instance.ClearPersistentData(); 
 
             // Enter additional configuration here
             DDNA.Instance.ClientVersion = CLIENT_VERSION;
@@ -50,12 +47,9 @@ namespace metagameTutorial
             // Send additional events to the Publisher MetaGame using the REST API
             // At the start of the game and other key points such as IAP purchase and gameEnd
             metagame_sender MetaGameSender = gameObject.GetComponent<metagame_sender>();
-
-            // Record MetaGame startup events
             MetaGameSender.StartMetaGame();
             /////////////////////////////////////////////////////////////////////////
         }
-
         
         
 
@@ -63,12 +57,29 @@ namespace metagameTutorial
         // Handle Button Clicks to simulate campaign check decision points.
         public void Button_PrimaryGameCampaignCheck_Clicked()
         {
-            PrimaryGameCampaignCheck();
+            PrimaryGameCampaignCheck("gameStarted");
         }
-        public void Button_MeatGameCampaignCheck_Clicked()
+        public void Button_MetaGameCampaignCheck_Clicked()
         {
-            // We'll do this inside metagame_sender.cs
-            MetaGameSender.MetaGameCampaignCheck();
+            MetaGameCampaignCheck("gameStarted");
+        }
+        public void Button_LevelUp_Clicked()
+        {
+            this.userLevel++;
+
+            // Send levelUp event to the primary game environment
+            GameEvent levelUp = new GameEvent("levelUp")
+                .AddParam("userLevel", this.userLevel)
+                .AddParam("levelUpName", string.Format("Level {0}",this.userLevel));
+            DDNA.Instance.RecordEvent(levelUp);
+            DDNA.Instance.Upload();
+
+            UpdatePlayerHUD();
+        }
+
+        public void Button_Reset_Clicked()
+        {
+            ResetPlayer();
         }
         ///////////////////////////////////////////////////////////////////
 
@@ -77,7 +88,107 @@ namespace metagameTutorial
             this.txtLevel.text = string.Format("Level : {0}", userLevel);
             this.txtCoins.text = string.Format("Coins : {0}", userCoins);
         }
+        private void ResetPlayer()
+        {
+            // For demo purposes... clear all data so we can see how campaigns react for a new player.
+            if(DDNA.Instance.isActiveAndEnabled)
+            {
+                DDNA.Instance.StopSDK();
+            }
+            DDNA.Instance.ClearPersistentData();
+            this.userLevel = 1;
+            this.userCoins = 0;
+           
+            this.Start();
+        }
 
+        ///////////////////////////////////////////////////////////////////////////////
+        // Primary Game Campaign Check
+        private void PrimaryGameCampaignCheck(string decisionPoint)
+        {
+            Debug.Log("Primary Campaign Check");
+            var engagement = new Engagement(decisionPoint)
+                 .AddParam("userLevel", this.userLevel);
+
+            // Make request to Engage In-Game campaign system to see 
+            // if there are any active campaigns for this player at this time
+            DDNA.Instance.RequestEngagement(engagement, (response) => {
+
+                ReactToCampaignResponse(response);
+            }, (exception) => {
+                Debug.Log("Engage reported an error: " + exception.Message);
+            });
+        }
+        ///////////////////////////////////////////////////////////////////////////////
+        // MetaGame Campaign Check
+        private void MetaGameCampaignCheck(string decisionPoint)
+        {
+            Debug.Log("MetaGame Campaign Check");
+            var engagement = new Engagement(decisionPoint)
+                 .AddParam("userLevel", this.userLevel);
+            metagame_sender MetaGameSender = gameObject.GetComponent<metagame_sender>();
+            // Make request to Engage In-Game campaign system to see 
+            // if there are any active campaigns for this player at this time
+            MetaGameSender.MetaGameRequestEngagement(engagement, (response) => {
+
+                ReactToCampaignResponse(response);
+            }, (exception) => {
+                Debug.Log("Engage reported an error: " + exception.Message);
+            });
+        }
+        ///////////////////////////////////////////////////////////////////////////////
+
+        ////////////////////////////////////////////////////////////////////////////////        
+        private void ReactToCampaignResponse(Engagement response)
+        {
+
+            ImageMessage imageMessage = ImageMessage.Create(response);
+
+            // Handle response
+            // Check we got an engagement with a valid image message.
+            if (imageMessage != null)
+            {
+                Debug.Log("Engage returned a valid image message.");
+
+                // This example will show the image as soon as the background
+                // and button images have been downloaded.
+                imageMessage.OnDidReceiveResources += () => {
+                    Debug.Log("Image Message loaded resources.");
+                    imageMessage.Show();
+                };
+
+                // Add a handler for the 'dismiss' action.
+                imageMessage.OnDismiss += (ImageMessage.EventArgs obj) => {
+                    Debug.Log("Image Message dismissed by " + obj.ID);
+                };
+
+                // Add a handler for the 'action' action.
+                imageMessage.OnAction += (ImageMessage.EventArgs obj) => {
+                    Debug.Log("Image Message actioned by " + obj.ID + " with command " + obj.ActionValue);
+
+                    // Look for specific values in the Button Action and act accordingly
+                    switch (obj.ActionValue.ToUpper())
+                    {
+                        case "GIFT":
+                            AwardGiftFromEngage(response);
+                            break;
+                        default:
+                            break;
+                    }
+                };
+
+                // Download the image message resources.
+                imageMessage.FetchResources();
+            }
+            else
+            {
+                Debug.Log("Engage didn't return an image message.");
+            }
+        }
+
+
+        ///////////////////////////////////////////////////////////////////////////               
+        // Award a gift to the player based on the response from an Engage campaign
         private void AwardGiftFromEngage(Engagement response)
         {
             object parametersObject;
@@ -89,6 +200,7 @@ namespace metagameTutorial
                    
                 if (parameters.ContainsKey("virtualCurrencyName") && parameters.ContainsKey("virtualCurrencyAmount"))
                 {
+                    // Looking for virtualCurrencyName == coins and virtualCurrencyAMount > 0
                     if (    parameters["virtualCurrencyName"]   != null &&
                             parameters["virtualCurrencyAmount"] != null &&
                             parameters["virtualCurrencyName"].ToString().ToLower() == "coins" && 
@@ -98,70 +210,11 @@ namespace metagameTutorial
                         this.userCoins += coins; 
                         DeltaDNA.Logger.LogDebug("Gifting player " + coins + " coins");
                     }
-
                 }
 
-                UpdatePlayerHUD();
-                
+                UpdatePlayerHUD();                
             }
         }
-
-        ///////////////////////////////////////////////////////////////////////////////
-        // Primary Game Campaign Check
-        private void PrimaryGameCampaignCheck()
-        {
-            Debug.Log("Primary Campaign Check");
-            var engagement = new Engagement("gameStarted")
-                 .AddParam("userLevel", this.userLevel); 
-
-
-            DDNA.Instance.RequestEngagement(engagement, (response) => {
-                ImageMessage imageMessage = ImageMessage.Create(response);
-
-                // Check we got an engagement with a valid image message.
-                if (imageMessage != null)
-                {
-                    Debug.Log("Engage returned a valid image message.");
-
-                    // This example will show the image as soon as the background
-                    // and button images have been downloaded.
-                    imageMessage.OnDidReceiveResources += () => {
-                        Debug.Log("Image Message loaded resources.");
-                        imageMessage.Show();
-                    };
-
-                    // Add a handler for the 'dismiss' action.
-                    imageMessage.OnDismiss += (ImageMessage.EventArgs obj) => {
-                        Debug.Log("Image Message dismissed by " + obj.ID);
-                    };
-
-                    // Add a handler for the 'action' action.
-                    imageMessage.OnAction += (ImageMessage.EventArgs obj) => {
-                        Debug.Log("Image Message actioned by " + obj.ID + " with command " + obj.ActionValue);
-
-                        switch(obj.ActionValue.ToUpper())
-                        {
-                            case "GIFT" :
-                                AwardGiftFromEngage(response);
-                                break; 
-                            default:
-                                break; 
-                                
-                        }
-                        
-                    };
-
-                    // Download the image message resources.
-                    imageMessage.FetchResources();
-                }
-                else
-                {
-                    Debug.Log("Engage didn't return an image message.");
-                }
-            }, (exception) => {
-                Debug.Log("Engage reported an error: " + exception.Message);
-            });
-        }
-
+   
     }
 }
